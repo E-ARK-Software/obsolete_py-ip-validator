6e6352e7f3467ca64d159806fbc34a6be5370705
rgraf <roman.graf@ait.ac.at>
2018-08-21 10:59:13 -0400

    initial draft for py-ip-validator project

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source ./venv/bin/activate
+python mets-validator/MetsValidator.py
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# coding=UTF-8
+#
+# BitCurator Access Webtools (Disk Image Access for the Web)
+# Copyright (C) 2014 - 2016
+# All rights reserved.
+#
+# This code is distributed under the terms of the GNU General Public
+# License, Version 3. See the text file "COPYING" for further details
+# about the terms of this license.
+#
+"""Setup for METS validator app."""
+from setuptools import setup
+
+setup(
+    name='mets-validator',
+    packages=['mets-validator'],
+    include_package_data=True,
+    install_requires=[
+        'lxml==3.7.3'
+    ],
+)
+
