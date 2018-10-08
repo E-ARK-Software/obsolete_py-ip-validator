07a20306f83cc3ca277c7367c6ac3cf1e84db088
rgraf <roman.graf@ait.ac.at>
2018-10-08 09:15:35 -0400

    implemented setup class for reading of validation rules before all tests

--- test_mets_root.py
+++ test_mets_root.py
@@ -6,22 +6,23 @@ from metsvalidator.mets_validator_impl import load_xml, validate
 class MetsRootTestCase(unittest.TestCase):
     """ Tests for METS root element """
 
+    @classmethod
+    def setUpClass(self):
+        self.rules = load_xml("metsvalidator/mets_validator.xml")
+
     def test_load_rules(self):
-        rules = load_xml("metsvalidator/mets_validator.xml")
-        validationResult, report = validate(rules, "metsvalidator/mets_root_element_ok.xml")
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_ok.xml")
         #print("is valid: " + str(validationResult))
         #print(type(report))
         #print(report)
         self.assertTrue(validationResult==True)
 
     def test_cif4_has_content_specification(self):
-        rules = load_xml("metsvalidator/mets_validator.xml")
-        validationResult, report = validate(rules, "metsvalidator/mets_root_element_csip_not_exists.xml")
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_csip_not_exists.xml")
         self.assertTrue(validationResult==False)
 
     def test_cif4_content_specification_value_error(self):
-        rules = load_xml("metsvalidator/mets_validator.xml")
-        validationResult, report = validate(rules, "metsvalidator/mets_root_element_csip_value_error.xml")
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_csip_value_error.xml")
         self.assertTrue(validationResult==False)
 
 if __name__ == '__main__':
