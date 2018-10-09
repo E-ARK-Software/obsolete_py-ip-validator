316e0f8316f854c39f23f54837743c4a66c273e5
rgraf <roman.graf@ait.ac.at>
2018-10-09 10:33:04 -0400

    implemented all METS root validation tests

--- test_mets_root.py
+++ test_mets_root.py
@@ -17,12 +17,32 @@ class MetsRootTestCase(unittest.TestCase):
         #print(report)
         self.assertTrue(validationResult==True)
 
-    def test_cif4_has_content_specification(self):
+    def test_csip1_check_root_element(self):
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip2_has_objid_specification(self):
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_objid_not_exists.xml")
+        self.assertTrue(validationResult==False)
+
+    def test_csip3_has_type_specification(self):
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_type_not_exists.xml")
+        self.assertTrue(validationResult==False)
+
+    def test_csip4_has_content_specification(self):
         validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_csip_not_exists.xml")
         self.assertTrue(validationResult==False)
 
-    def test_cif4_content_specification_value_error(self):
+    def test_csip4_content_specification_value_error(self):
         validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_csip_value_error.xml")
+        self.assertTrue(validationResult==False)
+
+    def test_csip5_content_specification_missing_other_type_error(self):
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_csip_other_type_not_exists.xml")
+        self.assertTrue(validationResult==False)
+
+    def test_csip6_content_specification_missing_profile_url_error(self):
+        validationResult, report = validate(self.rules, "metsvalidator/mets_root_element_profile_url_not_exists.xml")
         self.assertTrue(validationResult==False)
 
 if __name__ == '__main__':
