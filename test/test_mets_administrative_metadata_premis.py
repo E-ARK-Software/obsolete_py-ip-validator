b592160051d7316b338f80402990a53e60d8f8a3
Roman Graf <roman.graf@ait.ac.at>
2019-01-25 14:57:54 +0100

    Added validation checks for digiprovmd, id, status and mdref in administrative metadata in Premis format

--- test/test_mets_administrative_metadata_premis.py
+++ test/test_mets_administrative_metadata_premis.py
@@ -10,9 +10,21 @@ class MetsAdministrativeMetadataPremisTestCase(Base):
         validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_administrative_metadata_premis_element_ok.xml")
         self.assertTrue(validationResult==True)
 
-    #def test_csip31_check_administrative_metadata_premis_element(self):
-    #    validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_administrative_metadata_premis_element_not_exists.xml")
-    #    self.assertTrue(validationResult==True)
+    def test_csip31_check_administrative_metadata_premis_element(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_administrative_metadata_premis_element_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip32_check_administrative_metadata_premis_element_digiprovmd(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_administrative_metadata_premis_element_digiprovmd_validation.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip33_check_administrative_metadata_premis_element_digiprovmd_id(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_administrative_metadata_premis_element_digiprovmd_id_validation.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip34_check_administrative_metadata_premis_element_digiprovmd_status(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_administrative_metadata_premis_element_digiprovmd_status_validation.xml")
+        self.assertTrue(validationResult==True)
 
 
 
