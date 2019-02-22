460befb7c5f5a14b9eaef251bd2e827d1610ec7a
Roman Graf <roman.graf@ait.ac.at>
2019-02-22 15:48:59 +0100

    Finished initial validation of file section

--- test/test_file_premis.py
+++ test/test_file_premis.py
@@ -26,5 +26,77 @@ class MetsAdministrativeMetadataPremisTestCase(Base):
         validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_admid_not_exists.xml")
         self.assertTrue(validationResult==True)
 
+    def test_csip62_check_file_premis_element_contentinformationtype(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_contentinformationtype_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip63_check_file_premis_element_othercontentinformationtype(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_othercontentinformationtype_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip64_check_file_premis_element_use(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_use_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip65_check_file_premis_element_grp_id(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_id_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip66_check_file_premis_element_grp_file(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip67_check_file_premis_element_grp_file_id(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_id_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip68_check_file_premis_element_grp_file_mimetype(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_mimetype_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip69_check_file_premis_element_grp_file_size(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_size_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip70_check_file_premis_element_grp_file_created(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_created_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip71_check_file_premis_element_grp_file_checksum(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_checksum_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip72_check_file_premis_element_grp_file_checksum_type(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_checksum_type_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip73_check_file_premis_element_grp_file_owner_id(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_owner_id_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip74_check_file_premis_element_grp_file_adm_id(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_adm_id_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip75_check_file_premis_element_grp_file_dmd_id(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_dmd_id_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip76_check_file_premis_element_grp_file_flocat(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_flocat_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip77_check_file_premis_element_grp_file_flocat_loctype(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_flocat_loctype_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip78_check_file_premis_element_grp_file_flocat_type(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_flocat_xlink_type_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
+    def test_csip79_check_file_premis_element_grp_file_flocat_loctype(self):
+        validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_grp_file_flocat_xlink_href_not_exists.xml")
+        self.assertTrue(validationResult==True)
+
 
 
