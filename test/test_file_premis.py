8c4a558cb2140573d73769c67d44f87476d56e50
Roman Graf <roman.graf@ait.ac.at>
2019-02-28 17:11:53 +0100

    Implemented structmap

--- test/test_file_premis.py
+++ test/test_file_premis.py
@@ -1,10 +1,10 @@
 from base import Base
 from metsvalidator.mets_validator_impl import validate
 
-# In this class we test first the correct mets administrative metadata element in PREMIS format and then different error samples. 
+# In this class we test first the correct mets file metadata element in PREMIS format and then different error samples. 
 # If validation error was detected - validation result should be False.
-class MetsAdministrativeMetadataPremisTestCase(Base):
-    """ Tests for METS administrative metadata premis element """
+class MetsFileMetadataPremisTestCase(Base):
+    """ Tests for METS file metadata premis element """
 
     def test_load_rules(self):
         validationResult, report = validate(self.rules, self.SOURCES_PATH+"mets_file_premis_element/mets_file_premis_element_ok.xml")
